5ae01ec12fbfd4acaa0e540ef8cdee30a2a714c5
motonori19950629 <s2.k5.i2.a2.a@ezweb.ne.jp>
2021-08-18 21:27:34 +0900

    コメントアウトの編集

--- Ex1/ViewController.swift
+++ Ex1/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //5つの数字を入力するテキストフィールド
+    //5つの数字を入力するテキストフィールド（nil許容）
     @IBOutlet weak var num1: UITextField!
     @IBOutlet weak var num2: UITextField!
     @IBOutlet weak var num3: UITextField!
@@ -27,17 +27,17 @@ class ViewController: UIViewController {
     //足し算を実行するボタン
     @IBAction func runSum(_ sender: Any) {
         
-        //各変数を整数型に変換
+        //各String?をint?に変換しintに変換。未入力や文字列が入力された際は０に変換
         let intNum1 = Int(num1.text ?? "") ?? 0
         let intNum2 = Int(num2.text ?? "") ?? 0
         let intNum3 = Int(num3.text ?? "") ?? 0
         let intNum4 = Int(num4.text ?? "") ?? 0
         let intNum5 = Int(num5.text ?? "") ?? 0
     
-        //足し算処理：結果は整数型が格納される
+        //足し算処理：結果は整数型が格納される。
         let intSum = intNum1 + intNum2 + intNum3 + intNum4 +  intNum5
         
-        //結果をラベルに表示
+        //整数型を文字列に変換し結果をラベルに表示
         labelSum.text = "\(intSum)"
     }
 
